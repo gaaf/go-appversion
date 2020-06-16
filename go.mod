5930fc81f79c2258b19bb31ab95edf7b6bfaa530
Alex Hermann <alex@hexla.nl>
2020-06-16 14:53:53 +0200

    First version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gaaf/go-appversion
+
+go 1.14
---
+++ version.go
@@ -0,0 +1,25 @@
+package appversion
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"runtime"
+)
+
+var (
+	// Version is the application's version which must be set by the build system
+	Version = "unknown"
+	// BuildDate is the application' build timestamp wich must be set by the build system
+	BuildDate = "unknown"
+)
+
+// String formats the application's version and build date and returns the result as a string
+func String() string {
+	return fmt.Sprintf("%s %s (build with %s at %s)", path.Base(os.Args[0]), Version, runtime.Version(), BuildDate)
+}
+
+// Print formats the application's version and build date and prints the result
+func Print() {
+	fmt.Println(String())
+}
